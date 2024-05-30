cea2853146d551a4eabf229bc354d5bc5d3f3d2b
Rakhmatullo1 <omonovrahmatullo9@gmail.com>
2024-05-30 22:13:19 +0500

    Second commit

---
+++ findWordsContaining/src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> indexes = new ArrayList<>();
+
+        for(int i=0; i<words.length; i++) {
+            if(words[i].contains(String.valueOf(x))) {
+                indexes.add(i);
+            }
+        }
+
+        return indexes;
+    }
+}
