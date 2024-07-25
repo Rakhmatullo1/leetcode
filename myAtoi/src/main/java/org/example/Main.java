1c1cf8e594ff29db271bb6d112d9c74d28a0d642
Rakhmatullo1 <omonovrahmatullo9@gmail.com>
2024-07-25 14:00:37 +0500

    feat: Problem 8 solved

--- myAtoi/src/main/java/org/example/Main.java
+++ myAtoi/src/main/java/org/example/Main.java
@@ -2,11 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(myAtoi("  0000000000012345678"));
+        int result = myAtoi("-2147483649");
+        System.out.println(result);
     }
     public static int myAtoi(String s) {
         s =  s.trim();
-        String a="";
+        StringBuilder a= new StringBuilder();
         boolean isNegative = false;
 
         if(s.startsWith("+-")||s.startsWith("-+"))
@@ -23,39 +24,40 @@ public class Main {
 
         int[] parts = s.chars().toArray();
 
+        for(int i=0; i< parts.length; i++) {
+            if(parts[i] == 48) {
+                s = s.substring(1);
+            }else {
+                break;
+            }
+        }
 
-        for(int i=0; i<parts.length; i++) {
-            if(parts[i]<48 ||57 <parts[i]){
+        parts = s.chars().toArray();
+
+        for (int part : parts) {
+            if (part < 48 || 57 < part) {
                 break;
             }
 
-            a =  a + (char) parts[i];
+            a.append((char) part);
         }
 
-        if(a.isBlank()){
+        if(a.toString().isBlank()){
             return 0;
-        }
-
-        for(int i=0; i<a.length(); i++) {
-            if(i!=a.length()-1){
-                if(a.charAt(i)=='0' && a.charAt(i+1)!='0'){
-                    a = a.substring(i+1);
-                }
-            }
         }
 
         if(a.length()>String.valueOf(Integer.MAX_VALUE).length()){
             return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
         }
 
-        if(Long.parseLong(a)>Integer.MAX_VALUE){
-            return Integer.MAX_VALUE;
-        }
-
-        if(Long.parseLong(a)>Integer.MAX_VALUE && isNegative){
+        if(isNegative && Long.parseLong(a.toString()) >= (-1L)* Integer.MIN_VALUE){
             return Integer.MIN_VALUE;
         }
 
-        return isNegative ? (-1)*Integer.parseInt(a) : Integer.parseInt(a);
+        if(!isNegative && Long.parseLong(a.toString())>=Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+
+        return isNegative ? (-1)*Integer.parseInt(a.toString()) : Integer.parseInt(a.toString());
     }
 }
