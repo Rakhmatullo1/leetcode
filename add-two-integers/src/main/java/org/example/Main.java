5eee77655719712dee991f8e85dcb23797e3dbae
Rakhmatullo1 <omonovrahmatullo9@gmail.com>
2024-08-24 13:44:31 +0500

    feat: Problem 2235 solved

---
+++ add-two-integers/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * @author Rahmatullo Omonov
+ * Date: ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public int sum(int num1, int num2) {
+        return num1+num2;
+    }
+}
