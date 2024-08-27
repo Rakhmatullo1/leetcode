b6602a31e3ee80b85ed3f80535e4f5d9e668c498
Rakhmatullo1 <omonovrahmatullo9@gmail.com>
2024-08-27 23:02:36 +0500

    feat: Problem 2487 solved

--- removeNodes/src/main/java/org/example/Main.java
+++ removeNodes/src/main/java/org/example/Main.java
@@ -1,64 +1,47 @@
 package org.example;
 
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        ListNode tail = new ListNode(8);
-        ListNode fth = new ListNode(3,tail);
-        ListNode third = new ListNode(13, fth);
-        ListNode second = new ListNode(2, third);
-        ListNode head = new ListNode(5, second);
-
-        removeNodes(head);
-
+        Main main = new Main();
+        main.removeNodes(main.buildList(List.of(138,466,216,67,642,978,264,136,463,331,60,600,223,275,856,809,167,101,846,165,575,276,409,590,733,200,839,515,852,615,8,584,250,337,537,63,797,900,670,636,112,701,334,422,780,552,912,506,313,474,183,792,822,661,37,164,601,271,902,792,501,184,559,140,506,94,161,167,622,288,457,953,700,464,785,203,729,725,422,76,191,195,157,854,730,577,503,401,517,692,42,135,823,883,255,111,334,365,513,338,65,600,926,607,193,763,366,674,145,229,700,11,984,36,185,475,204,604,191,898,876,762,654,770,774,575,276,165,610,649,235,749,440,607,962,747,891,943,839,403,655,22,705,416,904,765,905,574,214,471,451,774,41,365,703,895,327,879,414,821,363,30,130,14,754,41,494,548,76,825,899,499,188,982,8,890,563,438,363,32,482,623,864,161,962,678,414,659,612,332,164,580,14,633,842,969,792,777,705,436,750,501,395,342,838,493,998,112,660,961,943,721,480,522,133,129,276,362,616,52,117,300,274,862,487,715,272,232,543,275,68,144,656,623,317,63,908,565,880,12,920,467,559,91,698)));
     }
 
-    public static ListNode removeNodes(ListNode head) {
-        head = reverse(head);
-        ListNode prev = head;
-        ListNode temp = head;
+    public ListNode removeNodes(ListNode head) {
+        List<Integer> nodes = new ArrayList<>();
 
-        int maxVal = 0;
-
-        while(!Objects.isNull(temp)) {
-            if(maxVal< temp.val) {
-                maxVal = temp.val;
-            } else {
-                deleteNode(prev, temp.val);
+        while (head != null) {
+            nodes.add(head.val);
+            head = head.next;
+        }
+        Collections.reverse(nodes);
+        List<Integer> temp = new ArrayList<>();
+        temp.add(nodes.get(0));
+        int len = 0;
+        for(int i=1; i< nodes.size(); i++) {
+            if(nodes.get(i)>=temp.get(len)) {
+                temp.add(nodes.get(i));
+                len++;
             }
-            temp = temp.next;
         }
-
-        return reverse(prev);
+        Collections.reverse(temp);
+        return buildList(temp);
     }
 
-    private static void deleteNode(ListNode head, int val) {
+    public ListNode buildList(List<Integer> array) {
+        ListNode head = new ListNode();
         ListNode temp = head;
-        ListNode prev= head;
 
-        while (!Objects.isNull(temp) && temp.val !=val)
-        {
-            prev = temp;
+        for (Integer num : array) {
+            temp.next = new ListNode(num);
             temp = temp.next;
         }
 
-        prev.next = temp.next;
-
-    }
-
-    private static ListNode reverse(ListNode head) {
-        ListNode prev = null;
-        ListNode current = head;
-        ListNode next = null;
-        while (current != null) {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-        head = prev;
+        head = head.next;
         return head;
     }
 }
