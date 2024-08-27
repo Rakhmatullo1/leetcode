cd69171ee11bf72bddf4f3fdbed8b3b3a24599e9
Rakhmatullo1 <omonovrahmatullo9@gmail.com>
2024-08-27 21:56:14 +0500

    feat: Problem 409 solved

--- longestPalindrome/src/main/java/org/example/Main.java
+++ longestPalindrome/src/main/java/org/example/Main.java
@@ -1,27 +1,59 @@
 package org.example;
 
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         Main main = new Main();
-        main.longestPalindrome("adam");
+        main.longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth");
     }
 
     public int longestPalindrome(String s) {
-      List<String> duplicates = Arrays.stream(s.split("")).distinct().toList();
-      String extraPart = "";
-      String[] parts =s.split("");
+        Map<Character, Integer> chars = new HashMap<>();
 
-      for(int i=0; i<parts.length; i++) {
-          if(!duplicates.contains(parts[i])) {
-              extraPart = parts[i];
-              break;
-          }
-      }
+        for (int i = 0; i < s.length(); i++) {
+            if (chars.containsKey(s.charAt(i))) {
+                chars.put(s.charAt(i), chars.get(s.charAt(i)) + 1);
+            } else {
+                chars.put(s.charAt(i), 1);
+            }
+        }
 
-      return 2*duplicates.size()-1;
+        int oddChars = 0;
+
+        for (Character key : chars.keySet()) {
+            if (isOdd(chars.get(key))) {
+                oddChars++;
+            }
+        }
+
+        if (oddChars == 0) {
+            return s.length();
+        }
+
+        if (oddChars == 1) {
+            return s.length();
+        }
+
+        int size = s.length();
+
+        List<Integer> sizes = chars.values().stream().sorted().filter(this::isOdd).collect(Collectors.toList());
+        Collections.reverse(sizes);
+        int len = sizes.size() - 1;
+        while (oddChars != 1) {
+            size = size - 1;
+            len--;
+            oddChars--;
+        }
+
+        return size;
+    }
+
+    private boolean isOdd(int num) {
+        return num % 2 != 0;
     }
 }
